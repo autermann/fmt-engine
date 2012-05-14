00230bfa7eb5d7e2d9116d3304002f52c1e1e0be
Christian Autermann <autermann@gmail.com>
2012-05-14 16:21:14 +0000

    git-svn-id: https://fmt.googlecode.com/svn/Engine/trunk@43 471a90e3-85d6-13bc-70e2-46c3232741f2

--- src/main/java/de/ifgi/fmt/web/filter/LogFilter.java
+++ src/main/java/de/ifgi/fmt/web/filter/LogFilter.java
@@ -6,12 +6,23 @@ import org.slf4j.LoggerFactory;
 import com.sun.jersey.spi.container.ContainerRequest;
 import com.sun.jersey.spi.container.ContainerRequestFilter;
 
+import de.ifgi.fmt.utils.constants.RESTConstants;
+
 public class LogFilter implements ContainerRequestFilter {
 	private static final Logger log = LoggerFactory.getLogger(LogFilter.class);
-	
+
 	@Override
 	public ContainerRequest filter(ContainerRequest request) {
-		log.debug("{} {}", request.getMethod(), request.getRequestUri());
+		if (log.isDebugEnabled()) {
+			String ct = request
+					.getHeaderValue(RESTConstants.HeaderParams.CONTENT_TYPE);
+			if (ct != null) {
+				log.debug("{} {}\n\tContent-Type: {}", new Object[] { 
+						request.getMethod(), request.getRequestUri(), ct });
+			} else {
+				log.debug("{} {}", request.getMethod(), request.getRequestUri());
+			}
+		}
 		return request;
 	}
 
