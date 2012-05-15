b985058a56992f358845f95ff34c3e8ae56385c8
Christian Autermann <autermann@gmail.com>
2012-05-15 14:48:22 +0000

    servlet simplifications
    git-svn-id: https://fmt.googlecode.com/svn/Engine/trunk@54 471a90e3-85d6-13bc-70e2-46c3232741f2

---
+++ src/main/java/de/ifgi/fmt/web/servlet/flashmobs/users/UsersServlet.java
@@ -0,0 +1,29 @@
+package de.ifgi.fmt.web.servlet.flashmobs.users;
+
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+import org.bson.types.ObjectId;
+
+import de.ifgi.fmt.model.User;
+import de.ifgi.fmt.utils.constants.RESTConstants.Paths;
+import de.ifgi.fmt.web.servlet.AbstractServlet;
+
+@Path(Paths.USERS_OF_FLASHMOB)
+public class UsersServlet extends AbstractServlet {
+	/*
+	 * /flashmobs/{fid}/users
+	 */
+
+	@GET
+	@Produces(MediaTypes.USER_LIST)
+	// get the participants of a flashmob
+	public List<User> getUsers(@PathParam(PathParams.FLASHMOB) ObjectId flashmob) {
+		// @ToDo
+		return null;
+	}
+}
