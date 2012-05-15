d56b632788e467732013e9646f44f4235b259b00
Christian Autermann <autermann@gmail.com>
2012-05-15 14:46:42 +0000

    servlet simplifications
    git-svn-id: https://fmt.googlecode.com/svn/Engine/trunk@51 471a90e3-85d6-13bc-70e2-46c3232741f2

--- src/main/java/de/ifgi/fmt/web/servlet/RootServlet.java
+++ src/main/java/de/ifgi/fmt/web/servlet/RootServlet.java
@@ -9,18 +9,17 @@ import org.codehaus.jettison.json.JSONObject;
 
 import de.ifgi.fmt.utils.Utils;
 import de.ifgi.fmt.utils.constants.JSONConstants;
+import de.ifgi.fmt.utils.constants.RESTConstants.Paths;
 
-@Path("/")
-public class Root extends AbstractServlet {
+@Path(Paths.ROOT)
+public class RootServlet extends AbstractServlet {
 
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public String getRoot() throws Exception {
-		log.debug("GET {}", getUriInfo().getRequestUri());
-		JSONObject j = new JSONObject().put(JSONConstants.USERS_KEY,
-				getUriInfo().getBaseUriBuilder().path(Paths.USERS)).put(
-				JSONConstants.FLASHMOBS_KEY,
-				getUriInfo().getBaseUriBuilder().path(Paths.FLASHMOBS));
+		JSONObject j = new JSONObject()
+			.put(JSONConstants.USERS_KEY, getUriInfo().getBaseUriBuilder().path(Paths.USERS))
+			.put(JSONConstants.FLASHMOBS_KEY, getUriInfo().getBaseUriBuilder().path(Paths.FLASHMOBS));
 		return Utils.toString(j);
 	}
 
